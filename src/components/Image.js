34a5eb7ce556269e0109692c20284a70cb2068fc
Huu Nhan <huunhankirigamer@gmail.com>
2020-08-11 22:14:14 +0700

    Add event click the image to show alert

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,15 +6,15 @@ class Image extends Component{
     // const { item } = this.props;
     let className = 'Image';
 
-    function imageClick() {
-      className += 'Image-size';
+    const imageClick = () => {
+      alert('Hihi');
     };
 
     return(
       <tr>
-        <td className={ className }><img src={this.props.item.col1} alt='img-1' onClick={imageClick} ></img></td>
-        <td className="Image"><img src={this.props.item.col2} alt='img-2'></img></td>
-        <td className="Image"><img src={this.props.item.col3} alt='img-3'></img></td>
+        <td className={className}><img src={this.props.item.col1} alt='img-1' onClick={()=>imageClick()}></img></td>
+        <td className={className}><img src={this.props.item.col2} alt='img-2' onClick={()=>imageClick()}></img></td>
+        <td className={className}><img src={this.props.item.col3} alt='img-3' onClick={()=>imageClick()}></img></td>
       </tr>
     );
   }
